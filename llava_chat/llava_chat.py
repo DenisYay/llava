a8b39937ebc734d14921eb0aef87dc6a5c201e56
dongreenberg <dongreenberg2@gmail.com>
2023-11-28 09:47:52 -0500

    Add llava_chat module

---
+++ llava_chat/llava_chat.py
@@ -0,0 +1,166 @@
+# LLaVA is an open-source chatbot trained by fine-tuning LLaMA/Vicuna on GPT-generated multimodal instruction-following data.
+# This example shows how to run inference of LLaVA-v1.5-7B (https://huggingface.co/liuhaotian/llava-v1.5-7b) using Runhouse (https://run.house).
+
+import runhouse as rh
+import torch
+from transformers import AutoTokenizer, BitsAndBytesConfig
+
+
+class LlavaModel(rh.Module):
+    def __init__(self, model_id="liuhaotian/llava-v1.5-7b"):
+        super().__init__()
+        self.model_id = model_id
+        self.model = None
+        self.tokenizer = None
+        self.image_processor = None
+        self.conv = None
+        self.conv_img = None
+        self.img_tensor = None
+        self.roles = None
+        self.stop_key = None
+
+    def load_models(self, model_id: str,
+                    device_map: str,
+                    load_in_8_bit: bool,
+                    **quant_kwargs) -> None:
+        from llava.utils import disable_torch_init
+        from llava.model import LlavaLlamaForCausalLM
+
+        quant_cfg = BitsAndBytesConfig(**quant_kwargs)
+        self.model = LlavaLlamaForCausalLM.from_pretrained(model_id,
+                                                           low_cpu_mem_usage=True,
+                                                           device_map=device_map,
+                                                           load_in_8bit=load_in_8_bit,
+                                                           torch_dtype=torch.float16,
+                                                           quantization_config=quant_cfg)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id,
+                                                       use_fast=False)
+        vision_tower = self.model.get_vision_tower()
+        vision_tower.load_model()
+        vision_tower.to(device='cuda')
+        self.image_processor = vision_tower.image_processor
+        disable_torch_init()
+
+    def load_image(self, img_path: str) -> None:
+        import requests
+        from PIL import Image
+        from io import BytesIO
+
+        """Load and process the image."""
+        if img_path.startswith('http') or img_path.startswith('https'):
+            response = requests.get(img_path)
+            self.conv_img = Image.open(BytesIO(response.content)).convert('RGB')
+        else:
+            self.conv_img = Image.open(img_path).convert('RGB')
+        self.img_tensor = self.image_processor.preprocess(self.conv_img,
+                                                          return_tensors='pt'
+                                                          )['pixel_values'].half().cuda()
+
+    def generate_answer(self, **kwargs) -> str:
+        from llava.mm_utils import tokenizer_image_token, KeywordsStoppingCriteria
+        from llava.constants import IMAGE_TOKEN_INDEX
+        import torch
+
+        """Generate an answer from the current conversation."""
+        raw_prompt = self.conv.get_prompt()
+        input_ids = tokenizer_image_token(raw_prompt,
+                                          self.tokenizer,
+                                          IMAGE_TOKEN_INDEX,
+                                          return_tensors='pt').unsqueeze(0).cuda()
+        stopping = KeywordsStoppingCriteria([self.stop_key],
+                                            self.tokenizer,
+                                            input_ids)
+        with torch.inference_mode():
+            output_ids = self.model.generate(input_ids,
+                                             images=self.img_tensor,
+                                             stopping_criteria=[stopping],
+                                             **kwargs)
+        outputs = self.tokenizer.decode(
+            output_ids[0, input_ids.shape[1]:]
+        ).strip()
+        self.conv.messages[-1][-1] = outputs
+
+        return outputs.rsplit('</s>', 1)[0]
+
+    def get_conv_text(self) -> str:
+        """Return full conversation text."""
+        return self.conv.get_prompt()
+
+    def start_new_chat(self,
+                       img_path: str,
+                       prompt: str,
+                       do_sample=True,
+                       temperature=0.2,
+                       max_new_tokens=1024,
+                       use_cache=True,
+                       **kwargs) -> str:
+        if not self.model:
+            self.load_models(model_id=self.model_id,
+                             device_map='auto',
+                             load_in_8_bit=True,
+                             bnb_8bit_compute_dtype=torch.float16,
+                             bnb_8bit_use_double_quant=True,
+                             bnb_8bit_quant_type='nf8')
+
+        """Start a new chat with a new image."""
+        from llava.conversation import conv_templates, SeparatorStyle
+        from llava.constants import DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+
+        conv_mode = "v1"
+        self.load_image(img_path)
+        self.conv = conv_templates[conv_mode].copy()
+        self.roles = self.conv.roles
+        first_input = (DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN +
+                       DEFAULT_IM_END_TOKEN + '\n' + prompt)
+        self.conv.append_message(self.roles[0], first_input)
+        self.conv.append_message(self.roles[1], None)
+        if self.conv.sep_style == SeparatorStyle.TWO:
+            self.stop_key = self.conv.sep2
+        else:
+            self.stop_key = self.conv.sep
+        answer = self.generate_answer(do_sample=do_sample,
+                                      temperature=temperature,
+                                      max_new_tokens=max_new_tokens,
+                                      use_cache=use_cache,
+                                      **kwargs)
+        return answer
+
+    def continue_chat(self,
+                      prompt: str,
+                      do_sample=True,
+                      temperature=0.2,
+                      max_new_tokens=1024,
+                      use_cache=True,
+                      **kwargs) -> str:
+        """Continue the existing chat."""
+        if self.conv is None:
+            raise RuntimeError("No existing conversation found. Start a new"
+                               "conversation using the `start_new_chat` method.")
+        self.conv.append_message(self.roles[0], prompt)
+        self.conv.append_message(self.roles[1], None)
+        answer = self.generate_answer(do_sample=do_sample,
+                                      temperature=temperature,
+                                      max_new_tokens=max_new_tokens,
+                                      use_cache=use_cache,
+                                      **kwargs)
+        return answer
+
+
+if __name__ == "__main__":
+    gpu = rh.ondemand_cluster(name='rh-a10x', instance_type='A10G:1')
+    # gpu.run(['pip install git+https://github.com/haotian-liu/LLaVA.git@786aa6a19ea10edc6f574ad2e16276974e9aaa3a'])
+    # Use get_or_to() if you want to reuse an existing instance of a running model
+    remote_llava_model = LlavaModel().to(system=gpu, name="llava-model")
+
+    ans = remote_llava_model.start_new_chat(img_path="https://upcdn.io/kW15bGw/raw/uploads/2023/09/22/file-387X.png",
+                                            prompt="How would I make this dish? Step by step please.")
+
+    print(f"\n\n... Answer ...\n"
+          f"{ans}"
+          f"\n\n")
+
+    ans = remote_llava_model.continue_chat(prompt="How does it taste like?")
+
+    print(f"\n\n... Answer ...\n"
+          f"{ans}"
+          f"\n\n")
